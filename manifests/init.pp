aabc44088b6fe9540eedcd1b5f694f9fb7a4b400
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-05-21 21:45:21 +0100

    Ensure web directory exists

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,9 @@
 class app-stack {
   package { "ImageMagick": ensure => installed }
+  file { "/web":
+      ensure => "directory",
+      owner => "deploy",
+      require => User['deploy'],
+      mode => 644,
+  }
 }
