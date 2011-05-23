45fdb290459c3163081a1a1cf4aab46d9702c6f0
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-05-23 10:59:48 +0100

    nodejs

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,4 +6,10 @@ class app-stack {
       require => User['deploy'],
       mode => 644,
   }
+  
+  package {"nodejs":
+    ensure => installed,
+    provider => rpm,
+    source => "http://download.opensuse.org/repositories/home:/SannisDev/CentOS_CentOS-5/x86_64/nodejs-0.4.7-2.1.x86_64.rpm",
+  }
 }
