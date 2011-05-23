3ea0661c750fac84ad9b3e08b4c3d55c287a5b81
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-05-23 11:14:05 +0100

    give full name to v8

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,7 +13,7 @@ class app-stack {
     source => "https://build.opensuse.org/package/binary?arch=x86_64&filename=libev4-4.01-6.1.x86_64.rpm&package=libev&project=home%3Aweberho%3Aqmail-ng-unstable&repository=CentOS-5",
   }
   
-  package {"libv8":
+  package {"libv8-3.x86_64":
     ensure => installed,
     provider => rpm,
     source => "http://download.opensuse.org/repositories/home:/SannisDev/CentOS_CentOS-5/x86_64/libv8-3-3.1.8-1.1.x86_64.rpm",
