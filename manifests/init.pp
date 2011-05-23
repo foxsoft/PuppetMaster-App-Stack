6815d1160dddda67206abc39807c17784f75aad7
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-05-23 11:04:04 +0100

    Require libv8

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,9 +7,16 @@ class app-stack {
       mode => 644,
   }
   
+  package {"libv8":
+    ensure => installed,
+    provider => rpm,
+    source => "http://download.opensuse.org/repositories/home:/SannisDev/CentOS_CentOS-5/x86_64/libv8-3-3.1.8-1.1.x86_64.rpm",
+  }
+  
   package {"nodejs":
     ensure => installed,
     provider => rpm,
     source => "http://download.opensuse.org/repositories/home:/SannisDev/CentOS_CentOS-5/x86_64/nodejs-0.4.7-2.1.x86_64.rpm",
+    require => Package["libv8"],
   }
 }
