4cd122cf6d7de70e925d5953f3d186bb24c338f7
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-05-24 16:34:44 +0100

    Bring nodejs back

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,16 +25,16 @@ class app-stack {
     source => "http://puppet.foxsoft.co.uk/centos/5/custom/libev-debuginfo-4.04-4.5.x86_64.rpm"
   }
   
-#  package {"libv8-3.x86_64":
-#    ensure => installed,
-#    provider => rpm,
-#    source => "http://download.opensuse.org/repositories/home:/SannisDev/CentOS_CentOS-5/x86_64/libv8-3-3.1.8-1.1.x86_64.rpm",
-#  }
-#  
-#  package {"nodejs":
-#    ensure => installed,
-#    provider => rpm,
-#    source => "http://download.opensuse.org/repositories/home:/SannisDev/CentOS_CentOS-5/x86_64/nodejs-0.4.7-2.1.x86_64.rpm",
-#    require => [Package["libv8-3.x86_64"], Package["libev"]],
-#  }
+  package {"libv8-3.x86_64":
+    ensure => installed,
+    provider => rpm,
+    source => "http://download.opensuse.org/repositories/home:/SannisDev/CentOS_CentOS-5/x86_64/libv8-3-3.1.8-1.1.x86_64.rpm",
+  }
+  
+  package {"nodejs":
+    ensure => installed,
+    provider => rpm,
+    source => "http://download.opensuse.org/repositories/home:/SannisDev/CentOS_CentOS-5/x86_64/nodejs-0.4.7-2.1.x86_64.rpm",
+    require => [Package["libv8-3.x86_64"], Package["libev"]],
+  }
 }
