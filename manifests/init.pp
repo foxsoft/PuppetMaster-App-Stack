c4793e03bbabbf000844d09eb98ccdbbbaa06474
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-05-24 21:14:27 +0100

    Rename package name

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,7 +35,7 @@ class app-stack {
     source => "http://download.opensuse.org/repositories/home:/SannisDev/CentOS_CentOS-5/x86_64/libv8-3-3.1.8-1.1.x86_64.rpm",
   }
   
-  package {"nodejs":
+  package {"nodejs.x86_64":
     ensure => installed,
     provider => rpm,
     source => "http://download.opensuse.org/repositories/home:/SannisDev/CentOS_CentOS-5/x86_64/nodejs-0.4.7-2.1.x86_64.rpm",
