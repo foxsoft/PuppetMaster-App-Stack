451db27736f86c61324f1f2774b45907a0c00207
Omar Qureshi <omar@omarqureshi.net>
2011-10-17 17:16:06 +0100

    Force Centos and 5

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class app-stack {
   yumrepo { "Omar":
-     baseurl => "http://yum.omarqureshi.net/$operatingsystem/$operatingsystemrelease/custom/$architecture",
+     baseurl => "http://yum.omarqureshi.net/Centos/5/custom/$architecture",
      descr => "Omar",
      enabled => 1,
      gpgcheck => 0
